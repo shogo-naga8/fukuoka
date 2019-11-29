228295da4c1c1e7107d931ba7089542d337d090a
shogo-naga8 <x08smjkcrXxx@gmail.com>
2019-11-29 21:39:08 +0900

    フロント画面変更

---
+++ app/assets/javascripts/bxslider.js
@@ -0,0 +1,16 @@
+  
+$(function(){
+  $('.bxslider').bxSlider({
+    auto: true,
+    slideWidth: 650,
+    minSlides: 1
+  });
+});
+
+// $('.btn-vertical-border').click(function(){
+//   alert('hello');
+// })
+
+// document.getElementById(".btn-vertical-border").click = function() {
+//   document.getElementById("text").innerHTML = "クリックされた！";
+// };
